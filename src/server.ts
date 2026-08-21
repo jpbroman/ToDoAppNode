69361b4ef049ab24d6ce7defeb677747eec697b5
Peter Broman <jp.broman@telia.com>
2026-08-21 09:19:20 +0200

    Lägger till GET och POST

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,38 @@ app.get("/api/todos", async (_req, res) => {
   res.json(todos);
 });
 
+app.get("/api/todos/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    const todo = await prisma.toDo.findUnique({
+        where: { id },
+    });
+
+    if (!todo) {
+        return res.status(404).json({
+            message: `Uppgiften med ID ${id} hittades inte.`,
+        });
+    }
+
+    res.json(todo);
+});
+
+app.post("/api/todos", async (req, res) => {
+    const { heading, note, doDate } = req.body;
+
+    const todo = await prisma.toDo.create({
+        data: {
+            heading,
+            note,
+            created: new Date(),
+            doDate: new Date(doDate),
+        },
+    });
+
+    res.status(201).json(todo);
+});
+
+
 app.listen(5163, () => {
   console.log("API kör på http://localhost:5163");
 });
